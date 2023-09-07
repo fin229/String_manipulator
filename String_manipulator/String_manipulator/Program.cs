7cbc8cfd80b08ece6c200f5091a1fb6298f0d054
Fineas <r0845664@student.thomasmore.be>
2023-09-07 22:17:27 +0200

    while lus toegevoegd en optie om het programma te stoppen toegevoegd

--- String_manipulator/String_manipulator/Program.cs
+++ String_manipulator/String_manipulator/Program.cs
@@ -17,25 +17,36 @@ namespace String_manipulator
                input = Console.ReadLine();
             } while (string.IsNullOrWhiteSpace(input));
 
-            Console.WriteLine("Hier is het menu met keuzes:"+Environment.NewLine);
-            Console.WriteLine("1: vervang lower case naar upper case." + Environment.NewLine + "2: vervang upper case naar lower case."+Environment.NewLine
-                +"3: geef de lengete van de string weer."+Environment.NewLine+"4: verwijder de spaties in de string.");
-            keuze=int.Parse(Console.ReadLine());
+            Console.WriteLine("Hier is het menu met keuzes:" + Environment.NewLine);
+            Console.WriteLine("1: vervang lower case naar upper case." + Environment.NewLine + "2: vervang upper case naar lower case." + Environment.NewLine
+                + "3: geef de lengete van de string weer." + Environment.NewLine + "4: verwijder de spaties in de string.");
+            Console.WriteLine("Voor het programma te stoppen geef 0 in");
+            keuze = int.Parse(Console.ReadLine());
 
-            switch (keuze)
+            while (keuze > 0)
             {
-                case 1:
-                    Console.WriteLine(LowerToUpper(input));
-                    break;
-                case 2:
-                    Console.WriteLine(UpperToLower(input));
-                    break;
-                case 3:
-                    Console.WriteLine(NumberOfChar(input));
-                    break;
-                case 4:
-                    Console.WriteLine(RemoveWhiteSpace(input));
-                    break;
+               
+
+                switch (keuze)
+                {
+                    case 1:
+                        Console.WriteLine(LowerToUpper(input));
+                        break;
+                    case 2:
+                        Console.WriteLine(UpperToLower(input));
+                        break;
+                    case 3:
+                        Console.WriteLine(NumberOfChar(input));
+                        break;
+                    case 4:
+                        Console.WriteLine(RemoveWhiteSpace(input));
+                        break;
+                }
+                Console.WriteLine("Hier is het menu met keuzes:" + Environment.NewLine);
+                Console.WriteLine("1: vervang lower case naar upper case." + Environment.NewLine + "2: vervang upper case naar lower case." + Environment.NewLine
+                    + "3: geef de lengete van de string weer." + Environment.NewLine + "4: verwijder de spaties in de string.");
+                Console.WriteLine("Voor het programma te stoppen geef 0 in");
+                keuze = int.Parse(Console.ReadLine());
             }
 
             static string LowerToUpper(string input)
@@ -50,7 +61,7 @@ namespace String_manipulator
 
             static int NumberOfChar(string input)
             {
-                string[] res=input.Split();
+                string[] res=input.Split(',');
                 return res.Length;
             }
 
