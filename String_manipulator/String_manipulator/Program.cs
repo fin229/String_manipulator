da6cdc17311d47ee202a1a23bcf399dce96df106
Fineas <r0845664@student.thomasmore.be>
2023-09-07 16:28:16 +0200

    menu met keuzes aangemaakt en switch statement aangemaakt voor het oproepen van de functies

--- String_manipulator/String_manipulator/Program.cs
+++ String_manipulator/String_manipulator/Program.cs
@@ -9,12 +9,34 @@ namespace String_manipulator
         static void Main(string[] args)
         {
             string input;
+            int keuze;
 
             do
             {
                 Console.WriteLine("Geef een string in");
                input = Console.ReadLine();
             } while (string.IsNullOrWhiteSpace(input));
+
+            Console.WriteLine("Hier is het menu met keuzes:"+Environment.NewLine);
+            Console.WriteLine("1: vervang lower case naar upper case." + Environment.NewLine + "2: vervang upper case naar lower case."+Environment.NewLine
+                +"3: geef de lengete van de string weer."+Environment.NewLine+"4: verwijder de spaties in de string.");
+            keuze=int.Parse(Console.ReadLine());
+
+            switch (keuze)
+            {
+                case 1:
+                    Console.WriteLine(LowerToUpper(input));
+                    break;
+                case 2:
+                    Console.WriteLine(UpperToLower(input));
+                    break;
+                case 3:
+                    Console.WriteLine(NumberOfChar(input));
+                    break;
+                case 4:
+                    Console.WriteLine(RemoveWhiteSpace(input));
+                    break;
+            }
 
             static string LowerToUpper(string input)
             {
